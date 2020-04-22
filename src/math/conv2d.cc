d4a220a7cae7706e3b2ae23355a68ac08f79c265
amazingyyc <amazingyyc@outlook.com>
2020-04-22 22:39:49 +0800

    remove unnecessary code

--- src/math/conv2d.cc
+++ src/math/conv2d.cc
@@ -52,76 +52,34 @@ void conv2d_float_impl(
   output_subsampling.width = stride[1];
   output_subsampling.height = stride[0];
 
-  if (1 == groups) {
-    // use nnpack directly
-    auto nnp_status = nnp_convolution_inference(
-      nnp_convolution_algorithm_auto,
-      nnp_convolution_transform_strategy_block_based,
-      input_channel,
-      output_channel,
-      input_size,
-      input_padding,
-      kernel_size,
-      output_subsampling,
-      input_ptr,
-      weight_ptr,
-      bias_ptr,
-      output_ptr,
-      // set threadpool
-      output.nnpack_threadpool(),
-      nullptr);
-  } else {
-    // multi-thread calculate result
-    Eigen::Barrier barrier((unsigned int)(groups));
-
-    auto block = [&barrier, &input_size, &input_padding, &kernel_size, &output_subsampling] (
-      int64_t input_channel,
-      int64_t output_channel,
-      float *input_ptr,
-      float *weight_ptr,
-      float *bias_ptr,
-      float *output_ptr) {
-      auto nnp_status = nnp_convolution_inference(
-        nnp_convolution_algorithm_auto,
-        nnp_convolution_transform_strategy_block_based,
-        input_channel,
-        output_channel,
-        input_size,
-        input_padding,
-        kernel_size,
-        output_subsampling,
-        input_ptr,
-        weight_ptr,
-        bias_ptr,
-        output_ptr,
-        // threadpool set nullptr
-        nullptr,
-        nullptr);
-
-      barrier.Notify();
-    };
-
-    int64_t input_channel_step = input_channel / groups;
-    int64_t output_channel_step = output_channel / groups;
-
-    for (int64_t g = 0; g < groups; ++g) {
-      eigen_device->enqueueNoNotification(
-        block,
-        input_channel_step,
-        output_channel_step,
-        input_ptr  + g * input_channel_step * input_height * input_width,
-        weight_ptr + g * output_channel_step * input_channel_step * kernel_height * kernel_width,
-        bias_ptr   + g * output_channel_step,
-        output_ptr + g * output_channel_step * output_height * output_width);
-    }
-
-    barrier.Wait();
+  // for save memory, when the input image is too big use gemm
+  auto convolution_algorithm = nnp_convolution_algorithm_auto;
+  if (input_height * input_width * output_channel >= 512 * 512 * 64) {
+    convolution_algorithm = nnp_convolution_algorithm_implicit_gemm;
   }
+  // use nnpack directly
+  auto nnp_status = nnp_convolution_inference(
+    convolution_algorithm,
+    nnp_convolution_transform_strategy_block_based,
+    input_channel,
+    output_channel,
+    input_size,
+    input_padding,
+    kernel_size,
+    output_subsampling,
+    input_ptr,
+    weight_ptr,
+    bias_ptr,
+    output_ptr,
+    // set threadpool
+    output.nnpack_threadpool(),
+    nullptr);
 }
 #endif
 
 void conv2d(const Tensor &input, const Tensor &weight, const Tensor &bias, Tensor &output, std::vector<size_t> stride, std::vector<size_t> padding, int64_t groups) {
   ARGUMENT_CHECK(1 == input.shape()[0] && 1 == output.shape()[0], "conv2d need batch is 1");
+  ARGUMENT_CHECK(1 == groups, "for now only support groups is 1");
 
   if (input.element_type().is<float>()) {
 #ifdef HAS_NNPACK
